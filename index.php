754eb3af3a0a10f10fe856cff4d92cff6fd5459f
alexmarch232 <alexmarch232@gmail.com>
2018-07-02 03:36:12 +0300

    Redefinition of CSS libraries wich using BootStrap 4, include links on Bootstrap 4 from a CDN, and also links on JQuery libraries. Include links on part sets for more responsive to mobile devices, touch zooming and rendering.

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<?php
+<!DOCTYPE html>
+	<html lang="en">
+		<head>
+			<title>Main page</title>
+			<meta name="alexmarch232" content="root" charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+			<link href="styles/style.css" rel="stylesheet" />
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+		</head>
 
+		<body>
+			<h1>MAIN PAGE</h1>
+			<br />
+		
+			<div><h2>
+
+
+<?php
 	session_start();
 	
 	if( !empty($_SESSION['username']) ) {
@@ -8,17 +27,9 @@
 	} else {
 ?>
 
-		<html>
-			<head>
-				<meta name="alexmarch232" content="root" charset="utf-8" />
-				<title>Main page</title>
-				<link href="styles/style.css" rel="stylesheet" />
-			</head>
+			</h2></div>
 
-			<body>
-				<h1>MAIN PAGE</h1>
-				<br />
-		
+
 				<form action="register.php">
 					<input id="inp" type="submit" value="Register" />
 				</form>
@@ -26,11 +37,12 @@
 				<form action="login.php">
 					<input id="inp" type="submit" value="Login" />		
 				</form>
-			</body>
-		</html>
+		</body>
+	</html>
+
 
 <?php
-
-	}
-	
+}
 ?>
+
+
